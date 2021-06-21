36812fd0548d1cba037c66638652dcdd8d47b8e3
user <h4099ju7ki8@yahoo.com.yw>
2021-06-21 23:21:21 +0800

    'serach'

--- routes/api.php
+++ routes/api.php
@@ -142,7 +142,7 @@ Route::get('/rates/{require}/{percent}/{upordown}',function ($require,$percent,$
             $picks = rate::where('rate', '<=', $percent)->where('url', $datum)->get();
         }
     }
-    $result = json_encode($picks);
+    $result = JSON.stringify($picks);
     return($result);
     //return($picks);
 });
