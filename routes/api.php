75539c9feea38fca1ff3213b5c4a3241905b3c86
user <h4099ju7ki8@yahoo.com.yw>
2021-06-22 20:24:31 +0800

    'search'

--- routes/api.php
+++ routes/api.php
@@ -142,7 +142,7 @@ Route::get('/rates/{require}/{percent}/{upordown}',function ($require,$percent,$
             $picks[] = rate::where('rate', '<=', $percent)->where('url', $datum)->get();
         }
     }
-    $result=json_encode((object)$picks,JSON_FORCE_OBJECT);
+    $result=json_decode($picks->content(), true);
     return($result);
 });
 
