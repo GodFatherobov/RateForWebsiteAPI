c29f5dfaa7e6afd69adb225f4cfba67c56648a99
user <h4099ju7ki8@yahoo.com.yw>
2021-06-22 20:28:45 +0800

    'search'

--- routes/api.php
+++ routes/api.php
@@ -142,7 +142,7 @@ Route::get('/rates/{require}/{percent}/{upordown}',function ($require,$percent,$
             $picks[] = rate::where('rate', '<=', $percent)->where('url', $datum)->get();
         }
     }
-    return($picks[0][1]);
+    return($picks[0][0]);
 });
 
 
