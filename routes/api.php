d4967bf059e232663ffb5020a01c8f5ad5d9ccb2
user <h4099ju7ki8@yahoo.com.yw>
2021-06-24 14:12:22 +0800

    'api'

--- routes/api.php
+++ routes/api.php
@@ -159,12 +159,12 @@ Route::get('/rates/{require}/{percent}/{upordown}',function ($require,$percent,$
     }
     if($upordown=='up') {
         foreach ($data as $datum) {
-            $picks[] = rate::where('rate', '>=', $percent)->where('url', $datum)->first()->get();
+            $picks[] = rate::where('rate', '>=', $percent)->where('url', $datum)->first();
         }
     }
     else {
         foreach ($data as $datum) {
-            $picks[]= rate::where('rate', '<=', $percent)->where('url', $datum)->first()->get();
+            $picks[]= rate::where('rate', '<=', $percent)->where('url', $datum)->first();
         }
     }
     return($picks);
